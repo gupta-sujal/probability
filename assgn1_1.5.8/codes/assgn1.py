98377fd303ed8b1dedaca3eacf3ff53d7c4ef697
gupta-sujal <sujalgupta484@gmail.com>
2023-08-08 14:43:55 +0530

    added  figure and used multline eqns

--- assgn1_1.5.8/codes/assgn1.py
+++ assgn1_1.5.8/codes/assgn1.py
@@ -1,9 +1,19 @@
 import numpy as np
 from sympy import init_printing
 init_printing(use_unicode=True)
-from sympy import symbols
 from sympy.matrices import Matrix
 from numpy import linalg as LA
+import sys                                          #for path to external scripts
+#sys.path.insert(0, '/home/user/txhome/storage/shared/gitlab/res2021/july/conics/codes/CoordGeo')        #path to my scripts
+sys.path.insert(0, '/home/sujalgupat484/Desktop/probability/math/codes/CoordGeo')        #path to my scripts
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#local imports
+from line.funcs import *
+
+from triangle.funcs import *
+from conics.funcs import circ_gen
 A=np.array([1,-1])
 B=np.array([-4,6])
 C=np.array([-3,-5])
@@ -32,6 +42,84 @@ print("the value of parameter k is ",k)
 D3=B+k*(C-B)
 print("the point of tangency of incircle by side BC is ",D3)
 #  to check we also check the value of dot product of ID3 and BC
-print("the dot product of ID3 and BC",abs(round(((D3-I)@(C-B),6))))
+#print("the dot product of ID3 and BC",abs(round(((D3-I)@(C-B),6))))
 #  so this comes out to be zero
 print("Hence we prove that side BC is tangent To incircle and also found the value of k!")
+
+# #Code by GVV Sharma
+# #December 7, 2019
+# #released under GNU GPL
+# #Drawing a triangle given 3 sides
+
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# image = mpimg.imread('exit-ramp.jpg')
+# plt.imshow(image)
+# plt.show()
+
+
+
+
+#Triangle coordinates
+[A,B,C] = tri_vert(a,b,c)
+A=np.array([1,-1])
+B=np.array([-4,6])
+C=np.array([-3,-5])
+a=np.linalg.norm(C-B)
+b=np.linalg.norm(A-C)
+c=np.linalg.norm(A-B)
+#Generating all lines
+x_AB = line_gen(A,B)
+x_BC = line_gen(B,C)
+x_CA = line_gen(C,A)
+x_BD = line_gen(B,D3)
+x_ID = line_gen(I,D3)
+
+
+#Generating the incircle
+[I,r] = icircle(A,B,C)
+x_icirc= circ_gen(I,radius)
+
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
+plt.plot(x_BD[0,:],x_BD[1,:],label='$BD$')
+plt.plot(x_ID[0,:],x_ID[1,:],label='$ID$')
+
+
+#Plotting the incircle
+plt.plot(x_icirc[0,:],x_icirc[1,:],label='$incircle$')
+
+#Labeling the coordinates
+tri_coords = np.vstack((A,B,C,I,D3)).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A','B','C','I','D3']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+
+#if using termux
+#plt.savefig('tri_sss.pdf')
+plt.savefig('/home/sujalgupat484/Desktop/probability/assgn1_1.5.8/figs/incircle.png')
+#subprocess.run(shlex.split("termux-open ./figs/tri_sss.pdf"))
+#else
+# image = mpimg.imread('tri_sss.png')
+# plt.imshow(image)
+#plt.show()
+
+
+
+
+
+
+
