eae1ae84845f56f1416e3d44e0f5c826334c7848
gupta-sujal <sujalgupta484@gmail.com>
2023-08-19 13:23:22 +0530

    changed python code

--- ncertq2/codes/main.py
+++ ncertq2/codes/main.py
@@ -3,29 +3,26 @@ import matplotlib.pyplot as plt
 from scipy.stats import bernoulli
 from scipy.stats import norm
 from scipy.stats import binom
-simlen=40
 n=2
-p=1/2
+simlen=int(1e2)
 
-# USING PROBABILITY MASS FUNCTION FOR BINOMIAL DISTRIBUTION
-binom_dist=binom(n,p)
-# probability of getting two heads in two coin flips
-two_head_prob = binom_dist.pmf(2)
-print(1-two_head_prob)
-print()
-# USING BINOMIAL DISTRIBUTION
-data_binom = binom.rvs(n,p,size=simlen) #Simulating the event of two heads
+#Probability of the event
+prob=1/2
+#Generating sample date using Bernoulli r.v.
+data_bern_mat = bernoulli.rvs(prob,size=(n,simlen))
+data_binom=np.sum(data_bern_mat, axis=0)
 print(data_binom)
 err_ind = np.nonzero(data_binom <=1) #checking probability condition of atmost one head
 err_n = np.size(err_ind) #computing the probability
-print(err_n/simlen)
-
+print("the practical probability of atmost one head is",err_n/simlen)
 print()
-# USING BERNOULLI DISTRIBUTION
-data_bern_mat = bernoulli.rvs(p,size=(n,simlen))
-data_binom=np.sum(data_bern_mat, axis=0)
-print(data_binom)
-err_ind = np.nonzero(data_binom <2) #checking probability condition
-err_n = np.size(err_ind) #computing the probability
-print(err_n/simlen)
+
+# using binomial to calculate theoretical probability of atmost one head.
+# USING PROBABILITY MASS FUNCTION FOR BINOMIAL DISTRIBUTION
+binom_dist=binom(n,prob)
+# probability of getting two heads in two coin flips
+two_head_prob = binom_dist.pmf(2)
+prob_atmost_one_head=1-two_head_prob
+print(prob_atmost_one_head)
+
 
