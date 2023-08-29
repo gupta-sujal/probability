96584e2adb47686fe47dcf565c3138420e348af0
gupta-sujal <sujalgupta484@gmail.com>
2023-08-29 19:59:05 +0530

    added figures and tables and used binomial

--- ncertq2/codes/main.py
+++ ncertq2/codes/main.py
@@ -1,28 +1,75 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import bernoulli
-from scipy.stats import norm
+import numpy as np
 from scipy.stats import binom
-n=2
-simlen=int(1e2)
 
-#Probability of the event
-prob=1/2
-#Generating sample date using Bernoulli r.v.
-data_bern_mat = bernoulli.rvs(prob,size=(n,simlen))
-data_binom=np.sum(data_bern_mat, axis=0)
-print(data_binom)
-err_ind = np.nonzero(data_binom <=1) #checking probability condition of atmost one head
-err_n = np.size(err_ind) #computing the probability
-print("the practical probability of atmost one head is",err_n/simlen)
-print()
- 	
-# using binomial to calculate theoretical probability of atmost one head.
-# USING PROBABILITY MASS FUNCTION FOR BINOMIAL DISTRIBUTION
-binom_dist=binom(n,prob)
-# probability of getting two heads in two coin flips
-two_head_prob = binom_dist.pmf(2)
-prob_atmost_one_head=1-two_head_prob
-print("the probability calculated using the binomial distribution is",prob_atmost_one_head)
+n = 2  # Number of coin tosses
+p = 0.5  # Probability of getting a head
+
+x = np.arange(0, n+1)
+pmf_values = binom.pmf(x, n, p)
+plt.stem(x, pmf_values)
+plt.xlabel('Number of Heads')
+plt.ylabel('Probability')
+plt.title('PMF of Number of Heads in Two Coin Tosses')
+plt.xticks(x)
+plt.grid(True)
+plt.show()
+
+cdf_values = binom.cdf(x, n, p)
+plt.figure(1)
+plt.plot(x, cdf_values, marker='o', linestyle='-', drawstyle='steps-post')
+plt.xlabel('Number of Heads')
+plt.ylabel('Cumulative Probability')
+plt.title('CDF of Number of Heads in Two Coin Tosses')
+plt.xticks(x)
+plt.grid(True)
+plt.savefig('/home/sujalgupat484/Desktop/probability/ncertq2/figs/figure2.png')
+
+# Generating vectors of successes
+x1 = np.random.randint(0, 2, size=10000)
+x2 = np.random.randint(0, 2, size=10000)
+
+# Calculate successful outcomes
+successful_outcomes = x1 + x2 
+
+# Count occurrences of each outcome
+counts = np.bincount(successful_outcomes, minlength=3)
+
+# Define possible values of h
+possible_values_of_h = np.arange(0, 3)
+
+# Calculate probabilities
+probabilities = counts / 10000
+
+#for plotting and simulation
+# Sample size
+simlen = 10000
+
+# Possible outcomes
+k_values = np.arange(0, 3)
+
+# Generate X1 and X2 without explicit loops
+y = np.random.randint(0, 2, size=(2, simlen))
+
+# Calculate X without loops
+X = np.sum(y, axis=0)
+
+# Find the frequency of each outcome
+unique, counts = np.unique(X, return_counts=True)
+
+# Simulated probability
+psim = counts / simlen
+
+X_axis = x = np.arange(0, n+1)
 
 
+# Plotting
+plt.figure(2)
+plt.stem(X_axis, psim, label='Simulation',linefmt='blue')
+plt.stem(X_axis, pmf_values, label='Analysis',linefmt='orange')
+plt.xlabel('$A$')  # Use 'k' instead of 'n'
+plt.ylabel('$p_{A}(k)$')  # Use 'k' instead of 'n'
+plt.legend()
+plt.title('Comaprision of theoretical and calculated PMF values')
+plt.grid()
+plt.savefig('/home/sujalgupat484/Desktop/probability/ncertq2/figs/figure1.png')
